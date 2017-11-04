19f3eb1c054be6c3667dbbf3396e225e62b238ba
hel352331 <alenka352331@gmail.com>
2017-11-05 01:33:07 +0200

    asking version

---
+++ create.js
@@ -0,0 +1,23 @@
+
+var names = prompt('Input your array:');
+var arr = names.split(', ');
+var i = 0;
+while(i < arr.length) {
+  i++;
+}
+alert( 'Your array is: ' + arr);
+
+
+
+
+// var arr = [];
+// var coma =',';
+// var i = 0;
+// var input = prompt('Input your array with separating every element by coma');
+// while (i < input.length ) {
+// if (input[i] == coma) {
+//   arr[i] = input[i];
+//   i++;
+// }
+// }
+// alert('Your array is: ' + arr);
